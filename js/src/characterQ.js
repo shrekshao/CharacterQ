f961faf4e0355910ecfc43a33461756b0d01378c
shrekshao <shrekshao@gmail.com>
2016-05-20 11:38:34 -0400

    fix typings

---
+++ js/src/characterQ.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+    
+    var CharacterQ = window.CharacterQ = {};
+    
+    CharacterQ.init = function() {
+        CharacterQ.scene = new THREE.Scene();
+        
+        //camera = new THREE.PerspectiveCamera();
+        var camera = new THREE.PerspectiveCamera();
+        
+    }
+})();
